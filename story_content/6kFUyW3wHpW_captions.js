c07c4411c2b7424974586b07c1bf6e8c34256298
TrainingContentTeam <jashley@lexipol.com>
2025-11-19 10:39:36 -0600

    Add files via upload

---
+++ story_content/6kFUyW3wHpW_captions.js
@@ -0,0 +1,1 @@
+window.globalLoadJsAsset('story_content/6kFUyW3wHpW_captions.js', {"captions":[{"langCode":"en-US","data":"WEBVTT%0D%0A%0D%0ANOTE%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.105.35604.0%0D%0A%0D%0A00:00:00.090%20--%3E%2000:00:02.800%0D%0AThe%20same%20button%20is%20used%20to%20return%20to%20full%20screen.%0D%0A%0D%0A"}]})
